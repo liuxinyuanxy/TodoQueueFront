eb4e1bfaa1f98e71a02b0e9d48c9ab010302de8a
T_horn <470898635@qq.com>
2022-06-27 12:29:22 +0800

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <TodoCard id={4} title={"qwq"}/>
+    <App />
   </React.StrictMode>
 );
 
