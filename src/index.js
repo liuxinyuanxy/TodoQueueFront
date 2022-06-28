b87e45529a1c69d6dfb727424883dcaf4944720e
liuxinyuanxy <liuxinyuanxy@163.com>
2022-06-28 14:47:01 +0800

    update todo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TodoCard from './Todo';
+import { NewTodo, TodoCard, NewTemplate } from './Todo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <TodoCard id={4} title={"qwq"}/>
+      <NewTodo />
   </React.StrictMode>
 );
 
